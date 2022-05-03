9e28191dbd384f7afeacbad39fbfe3e21fb770ad
Gitesh <gkumarjha@adobe.com>
2022-05-03 21:00:31 +0530

    first commit

---
+++ src/main/java/org/example/MainClass.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MainClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+
+    @GetMapping("hello")
+    public String isExistingUser(){
+        return "Github - CI/CD";
+    }
+
+}
+
